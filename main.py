ec763638861d445f0c1bd07ccbe7cec118d83707
pandalow <zxj000hugh@gmail.com>
2025-03-24 17:39:59 +0000

    change model to smaller one

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from fastapi.middleware.cors import CORSMiddleware
 import re
 from pydantic import BaseModel
 
+
 app = FastAPI()
 
 vector_store = None
 graph = None
 
-# 配置 CORS
+# config CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://pandalow.github.io/"],  #允许 Vite 前端的地址
+    allow_origins=["http://localhost:5173", "https://pandalow.github.io/"], 
     allow_credentials=True,
-    allow_methods=["*"],  # 允许所有方法（GET, POST, etc.）
-    allow_headers=["*"],  # 允许所有 headers
+    allow_methods=["*"],  # allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # allow all headers
 )
 
 class Question(BaseModel):
