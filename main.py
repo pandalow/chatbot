6688cd9d3d1dbf5ab1740d1c3edeb52f6135f5ea
pandalow <zxj000hugh@gmail.com>
2025-03-25 12:43:14 +0000

    fixing app not define bug

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
 
+app = FastAPI()
+
 limiter = Limiter(key_func=get_remote_address, default_limits=["10/hour"])
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
-app = FastAPI()
+
 
 vector_store = None
 graph = None
