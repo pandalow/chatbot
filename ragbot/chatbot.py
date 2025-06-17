48a9a17c66b7bc8ca7197535ea94e61d6b168048
pandalow <zxj000hugh@gmail.com>
2025-06-17 14:53:21 +0100

    adding open pkg

--- ragbot/chatbot.py
+++ ragbot/chatbot.py
@@ -1,4 +1,4 @@
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_chroma import Chroma
 
 from langchain_community.document_loaders import JSONLoader
